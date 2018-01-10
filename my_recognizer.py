fceff466323f719fb08d98ea925f30c833448fa3
Henry Chan <henry.chan@tigerspike.com>
2018-01-10 00:12:48 -0500

    Implemented DIC

--- my_recognizer.py
+++ my_recognizer.py
@@ -21,5 +21,25 @@ def recognize(models: dict, test_set: SinglesData):
     probabilities = []
     guesses = []
     # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    for test_word in test_set.wordlist:
+        dic = {}
+        idx = test_set.wordlist.index(test_word)
+        test_X, test_lengths = test_set.get_item_Xlengths(idx)
+        best_guess = None
+        best_score = float("-inf")
+        for current_word in models:
+            current_model = models[current_word]
+            if (current_word in test_set.wordlist):
+                try:
+                    score = current_model.score(test_X, test_lengths)
+                    dic[current_word] = score
+                    if score > best_score:
+                        best_score = score
+                        best_guess = current_word
+                except:
+                    continue
+        probabilities.append(dic)
+        guesses.append(best_guess)
+
+    return probabilities, guesses
